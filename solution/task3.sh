5c5b7f2a1321f4a322228bea4768a2af95f68e37
DanCpp <danila.ushakov.08@mail.ru>
2025-09-06 15:35:21 +0700

    update task3

--- solution/task3.sh
+++ solution/task3.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-find . -name "*.txt" -type f -not -name "temp.txt" -exec basename {} \; > temp.txt
-sort temp.txt
-rm temp.txt
+find . -name "*.txt" -type f -not -name "temp.txt" -exec basename {} \; | sort
